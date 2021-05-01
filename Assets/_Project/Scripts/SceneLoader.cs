ee2b37ca53c36e43a4212989174beb1b5e0913b9
Ice_trooper <trooperice@gmail.com>
2021-05-01 21:44:02 +0200

    added scene loader script and implemented it

---
+++ Assets/_Project/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadNext()
+    {
+        int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if(nextIndex >= SceneManager.sceneCountInBuildSettings) nextIndex = 0;
+        SceneManager.LoadScene(nextIndex);
+    }
+}
